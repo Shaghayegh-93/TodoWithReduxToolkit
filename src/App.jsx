eb8bd796166da15256d8108815cad2b78c121f01
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2023-10-17 20:45:45 +0100

    render TodoApp

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-
-import './App.css'
+import "./App.css";
+import TodoApp from "./components/TodoApp";
 
 function App() {
-
   return (
-    <>
-    
-    </>
-  )
+    <div className="max-w-5xl m-auto">
+      <TodoApp />
+    </div>
+  );
 }
 
-export default App
+export default App;
